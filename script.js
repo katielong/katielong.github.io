30073748680959241507aea9ce9e8e1386d3f26c
katielong <yl3239@columbia.edu>
2016-09-05 16:58:55 -0400

    plugin to beautify

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){	
-	$(".box").mouseenter(function(){
-		var newdiv = $(this).html();
+$(document).ready(function() {
+    $(".box").mouseenter(function() {
+        var newdiv = $(this).html();
 
-		$(newdiv).css({
-			position: "absolute",
-			width: "100%",
-			height: "100%",
-			top: "0",
-			left: "0",
-			borderStyle:"solid",
-			borderWidth:"0.5em",
-			borderColor:"black",
-			backgroundColor:"black",
-			opacity:"0.9",
-			zIndex:"2"
-		}).attr("id", "current")
-		.appendTo($(this).css("position","relative"));
+        $(newdiv).css({
+                position: "absolute",
+                width: "100%",
+                height: "100%",
+                top: "0",
+                left: "0",
+                borderStyle: "solid",
+                borderWidth: "0.5em",
+                borderColor: "black",
+                backgroundColor: "black",
+                opacity: "0.9",
+                zIndex: "2"
+            }).attr("id", "current")
+            .appendTo($(this).css("position", "relative"));
 
-	}).mouseleave(function(){
-		$("#current").remove();
-	});
+    }).mouseleave(function() {
+        $("#current").remove();
+    });
 });
